47a9172f88d2727cbd4b48215ae22baca40ba525
IskanderValiev <iskand.valiev@yandex.ru>
2018-01-11 13:45:48 +0300

    something changed

--- src/main/java/ru/isko/controllers/MenuController.java
+++ src/main/java/ru/isko/controllers/MenuController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import ru.isko.models.Category;
 import ru.isko.repositories.CategoryRepository;
 import ru.isko.repositories.DishRepository;
 import ru.isko.services.AuthenticationService;
@@ -40,8 +41,9 @@ public class MenuController {
 
     @GetMapping("/menu/{category}")
     public String getDishesByCategory(@ModelAttribute("model")ModelMap model, @PathVariable("category")String category) {
+        Category selectedCategory = categoryRepository.findByTitle(category);
         model.addAttribute("dishes", categoryRepository.findByTitle(category));
-        return "menu";
+        return "redirect:/files/"+selectedCategory.getImage().get(0).getStorageFileName();
     }
 
     @GetMapping("/admin/menu/add")
